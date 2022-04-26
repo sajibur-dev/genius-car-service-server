b9017d53f0d2be8fc17e028575abf4437a8e2f0f
sajibur-dev <sajibur.dev@gmail.com>
2022-04-26 13:41:45 +0600

    port changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
